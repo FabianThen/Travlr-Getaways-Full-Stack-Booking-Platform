152cc7cc5363b92b52b1a56c04d45f626cd685ce
FabianThen <fabianthen@gmail.com>
2026-03-10 10:05:29 -0400

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
+const indexRouter = require('./app_server/routes/index');
+const travelRouter = require('./app_server/routes/travel');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
+// View engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Register partials
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
+
+// Routes
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
+
+// Start server
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
+module.exports = app;
 
---
+++ app_server/controllers/travel.js
@@ -0,0 +1,6 @@
+// GET method route - travel page
+const travel = (req, res) => {
+    res.render('travel');
+};
+
+module.exports = { travel };
